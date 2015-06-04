26b8e8d4d352f8709b925b268e5b846a28f08037
JongChanNam <JongChanNam@namjongchan-ui-MacBook-Pro.local>
2015-06-04 19:02:43 +0900

    Initial commit

---
+++ app/build/generated/source/r/androidTest/debug/com/garamsoft/alarm/ActivityAlarmedTimeShowTest.java
@@ -0,0 +1,13 @@
+package com.garamsoft.alarm;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by JongChanNam on 2015. 6. 4..
+ */
+public class ActivityAlarmedTimeShowTest extends TestCase {
+
+    public void testOnCreate() throws Exception {
+
+    }
+}
---
+++ app/src/main/java/com/garamsoft/alarm/AdapterAlarm.java
@@ -0,0 +1,80 @@
+package com.garamsoft.alarm;
+
+import java.util.ArrayList;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Toast;
+
+import com.garamsoft.alarm.LinearLayoutSingleAlarmItem.OnRemoveButtonClickListner;
+
+public class AdapterAlarm extends BaseAdapter {
+	
+	Context mContext;
+	ArrayList<String> mData;
+	LayoutInflater mInflate;
+	ArrayList<AlarmData> arrayListAlarmDatas;
+	
+	public AdapterAlarm(Context context, ArrayList<AlarmData> arrayListAlarmDatas) {
+		mContext = context;
+		this.arrayListAlarmDatas = arrayListAlarmDatas;
+		mInflate = LayoutInflater.from(mContext);
+	}
+
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return arrayListAlarmDatas.size();
+	}
+
+	@Override
+	public Object getItem(int position) {
+		// TODO Auto-generated method stub
+		return position;
+	}
+
+	@Override
+	public long getItemId(int position) {
+		// TODO Auto-generated method stub
+		return arrayListAlarmDatas.get(position).reqCode;
+	}
+	
+	public boolean removeData(int position){
+		arrayListAlarmDatas.remove(position);
+		notifyDataSetChanged();
+		return false;
+	}
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		LinearLayoutSingleAlarmItem layoutSingleAlarmItem = (LinearLayoutSingleAlarmItem) convertView;
+		
+		if (layoutSingleAlarmItem == null) {
+			layoutSingleAlarmItem = new LinearLayoutSingleAlarmItem(mContext);
+			layoutSingleAlarmItem.setOnRemoveButtonClickListner(onRemoveButtonClickListner);
+		}
+		layoutSingleAlarmItem.setData(arrayListAlarmDatas.get(position), position);
+		return layoutSingleAlarmItem;
+	}
+	
+	OnRemoveButtonClickListner onRemoveButtonClickListner = new OnRemoveButtonClickListner() {
+		
+		@Override
+		public void onClicked(int hh, int mm, int reqCode, int position) {
+			Toast.makeText(mContext, "position : "+position + " reqCode :"+reqCode, 0).show();
+			 AlarmManager alarmManager = (AlarmManager)mContext.getSystemService(Context.ALARM_SERVICE);
+//			    Intent i = new Intent(mContext ,AlarmTestForHaruActivity.class);
+			 	Intent intent = new Intent(mContext, ActivityAlarmedTimeShow.class);
+			    Toast.makeText(mContext, "reqCode : "+reqCode, 0).show();
+			    PendingIntent pi = PendingIntent.getActivity(mContext, reqCode, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+			    alarmManager.cancel(pi);
+			removeData(position);
+		}
+	};
+
+}
